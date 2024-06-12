258649e7df966535a225241c68151ae228687b1f
Casyburnsed <Casyburnsed@gmail.com>
2024-06-12 00:19:20 -0700

    create html, css, and initial javascript

---
+++ script.js
@@ -0,0 +1,98 @@
+const data = [
+    {
+        id: 1,
+        name: "Alakazam",
+        img: ,
+        hp: "80",
+        type: "Psychic",
+    },
+
+    {
+        id: 2,
+        name: "Beedrill",
+        img: ,
+        hp: "80",
+        type: "Flying",
+    },
+
+    {
+        id: 3,
+        name: "Blastoise",
+        img: ,
+        hp: "100",
+        type: "Water",
+    },
+
+    {
+        id: 4,
+        name: "Chansey",
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+    {
+        id: 5,
+        name: ,
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+    {
+        id: 6,
+        name: ,
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+    {
+        id: 7,
+        name: ,
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+    {
+        id: 8,
+        name: ,
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+    {
+        id: 9,
+        name: ,
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+    {
+        id: 10,
+        name: ,
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+    {
+        id: 11,
+        name: ,
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+    {
+        id: 12,
+        name: ,
+        img: ,
+        hp: ,
+        type: ,
+    },
+
+]
